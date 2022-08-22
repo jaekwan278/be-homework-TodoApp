9c737f036bea3139d1b4e97a781d4de7f85a04b8
jaekwan278 <wprhks536@gmail.com>
2022-08-23 00:59:58 +0900

    To-do Application !

---
+++ src/main/java/com/codestates/TodoApp/TodoController.java
@@ -0,0 +1,15 @@
+package com.codestates.TodoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
+public class TodoController {
+    @GetMapping
+    public String helloWorld(){
+        return "To-do Application !";
+    }
+}
